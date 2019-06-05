f5d82f212e0e8243b765a00a07a50584108dcd24
Ivan5 <converse_ivan@hotmail.com>
2019-06-05 18:08:50 -0500

    Refactor code in server

--- controllers/ImageController.js
+++ controllers/ImageController.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const formidable = require("formidable");
-const ImageModule = require("../models/ImageModule");
+const ImageModel = require("../models/ImageModel");
 const ObjectId = require("mongoose").Types.ObjectId;
 
 let directory = path.join(__dirname, "../public/images");
@@ -19,9 +19,9 @@ module.exports = {
     form.parse(req, (err, fields, files) => {
       if (err) throw err;
 
-      let ImageObject = new ImageModule({
+      let ImageObject = new ImageModel({
         title: fields.title,
-        filename: fields.image.name
+        filename: files.image.name
       });
 
       fs.rename(
@@ -33,12 +33,27 @@ module.exports = {
           ImageObject.save((err, image) => {
             if (err) throw err;
 
-            console.log(image);
             res.setHeader("Content-Type", "application/json");
             res.status(200).send(image);
           });
         }
       );
+    });
+  },
+  retrieveImages: (req, res) => {
+    ImageModel.find((err, images) => {
+      if (err) throw err;
+
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).send(images);
+    });
+  },
+  removeImage: (req, res) => {
+    fs.unlinkSync(path.join(directory, req.body.filename));
+    ImageModel.remove({ _id: ObjectId(req.body._id) }, (err, result) => {
+      if (err) throw err;
+
+      res.status(200).send(result);
     });
   }
 };
